8742e3636f0430edf4bdb04c2458599930c718d5
JerinXplore <jerinjerin101325@gmail.com>
2025-03-01 12:05:10 +0600

    added updated date to the date card

--- script/datetime.js
+++ script/datetime.js
@@ -1,3 +1,10 @@
-let now = new Date();
-let dateTime = now.toLocaleString();
-document.getElementById('date').innerHTML = dateTime;
+    const updatedDate = new Date();
+    
+    const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    const months = ['January','February','March','April','May','June','July','August','September','October','Nonember','December'];
+    const updatedDayName = days[updatedDate.getDay()];
+    const updatedDay = updatedDate.getDate();
+    const updatedMonth = months[updatedDate.getMonth()];
+    const updatedYear = updatedDate.getFullYear();
+    const wholeUpdatedDate = `${updatedDayName}   ,${updatedDay} ${updatedMonth} ${updatedYear}`;
+    document.getElementById('date').innerText = wholeUpdatedDate;
